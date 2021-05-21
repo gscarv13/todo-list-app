b23efa67096e4d5dcb4e1ff4f15fc32c4ca79cdc
Gus <gsc13@tuta.io>
2021-05-21 18:26:59 -0300

    Update main and task, Add project form
    Minor changes on html tags and function names

--- src/scripts/builders.js
+++ src/scripts/builders.js
@@ -1,5 +1,3 @@
-import Task from './taskClass';
-
 const top = () => {
   const wrapper = document.createElement('div');
   const h1 = document.createElement('h1');
@@ -21,58 +19,49 @@ const container = () => {
 const navigation = () => {
   const nav = document.createElement('nav');
   const h2 = document.createElement('h2');
-  const tasksContainer = document.createElement('div');
+  const projectsContainer = document.createElement('div');
   const addProjectButton = document.createElement('button');
+  const formContainer = document.createElement('div');
 
   h2.textContent = 'Projects';
 
-  tasksContainer.id = 'task-list';
-  tasksContainer.classList.add('mt-5');
+  formContainer.id = 'projectFormContainer';
+
+  projectsContainer.id = 'projectsList';
+  projectsContainer.classList.add('mt-5');
 
   addProjectButton.textContent = 'Create Project';
   addProjectButton.id = 'createProject';
   addProjectButton.classList.add('button', 'button-info', 'is-small', 'mt-5');
 
-  nav.append(h2, tasksContainer, addProjectButton);
+  nav.append(h2, formContainer, projectsContainer, addProjectButton);
 
   return nav;
 };
 
 const main = () => {
   const main = document.createElement('main');
+  const mainHeader = document.createElement('div');
   const h2 = document.createElement('h2');
-  const tableWrapper = document.createElement('div');
-  const table = document.createElement('table');
-  const thead = document.createElement('thead');
-  const tr = document.createElement('tr');
-  const thCheckBox = document.createElement('th');
-  const thTitle = document.createElement('th');
-  const thDate = document.createElement('th');
-  const thDelete = document.createElement('th');
-  const tbody = document.createElement('tbody');
   const addNew = document.createElement('button');
+  const formWrapper = document.createElement('div');
+  const tasksContainer = document.createElement('div');
+
+  mainHeader.classList.add('is-flex', 'is-justify-content-space-between', 'my-5');
 
   h2.classList.add('is-size-5', 'has-text-weight-medium');
   h2.textContent = 'Tasks';
-
-  tableWrapper.classList.add('columns', 'is-centered', 'mt-5');
-  table.classList.add('table', 'is-striped', 'mx-auto');
-
-  tbody.id = 'tasksOutput';
-
-  thTitle.textContent = 'Title';
-  thDate.textContent = 'Date';
-  thDelete.textContent = 'Delete';
 
   addNew.classList.add('button', 'is-primary', 'is-small', 'has-text-dark');
   addNew.id = 'createTask';
   addNew.textContent = 'Create New Task';
 
-  tr.append(thCheckBox, thTitle, thDate, thDelete);
-  thead.appendChild(tr);
-  table.append(thead, tbody);
-  tableWrapper.appendChild(table);
-  main.append(h2, table, addNew);
+  formWrapper.id = 'formContainer';
+  tasksContainer.id = 'tasksContainer';
+  tasksContainer.classList.add('is-flex', 'is-flex-wrap-wrap', 'is-justify-content-center', 'mt-5');
+
+  mainHeader.append(h2, addNew);
+  main.append(mainHeader, formWrapper, tasksContainer);
 
   return main;
 };
@@ -152,7 +141,7 @@ const fullPage = () => {
   return content;
 };
 
-const project = (project, projectsArray) => {
+const projectLink = (project, projectsArray) => {
   const link = document.createElement('a');
   const linkContainer = document.createElement('p');
 
@@ -163,19 +152,76 @@ const project = (project, projectsArray) => {
   return linkContainer;
 };
 
-const task = (project) => {
-  const title = document.querySelector('#title');
-  const description = document.quesrySelector('#description');
-  const date = document.quesrySelector('#date');
-  const priority = document.quesrySelector('#priority');
+const task = (task) => {
+  const card = document.createElement('div');
+  const cardHeader = document.createElement('div');
+  const taskTitle = document.createElement('p');
+  const contentWrapper = document.createElement('div');
+  const contentContainer = document.createElement('div');
+  const description = document.createElement('p');
+  const date = document.createElement('p');
+  const cardFooter = document.createElement('div');
+  const priority = document.createElement('a');
+  const deleteOption = document.createElement('a');
+  const doneOption = document.createElement('a');
 
-  if (title.value !== '' && description.value !== '') {
-    const task = new Task(title.value, date.value, description.value, priority.value);
-    project.addTask(task);
-  }
-  return task;
+  card.classList.add('card');
+  // card.setAttribute('data-attribute', );
+
+  cardHeader.classList.add('card-header');
+  taskTitle.classList.add('card-header-title');
+  taskTitle.id = 'taskTitle';
+  taskTitle.textContent = task.title;
+
+  contentWrapper.classList.add('card-content');
+  contentContainer.classList.add('content');
+  description.id = 'description';
+  description.textContent = task.description;
+  date.id = 'date';
+  date.textContent = task.dueDate;
+
+  cardFooter.classList.add('card-footer');
+  deleteOption.classList.add('card-footer-item');
+  deleteOption.href = '#';
+  deleteOption.textContent = 'Delete';
+  doneOption.classList.add('card-footer-item');
+  doneOption.href = '#';
+  doneOption.textContent = 'Done';
+  priority.classList.add('card-footer-item');
+  priority.textContent = task.priority;
+
+  cardHeader.append(taskTitle);
+  contentContainer.append(description, date);
+  contentWrapper.append(contentContainer);
+  cardFooter.append(doneOption, deleteOption, priority);
+  card.append(cardHeader, contentWrapper, cardFooter);
+
+  return card;
+};
+
+const projectForm = () => {
+  const form = document.createElement('form');
+  const label = document.createElement('label');
+  const input = document.createElement('input');
+  const button = document.createElement('button');
+
+  form.classList.add('mt-5', 'box');
+  form.id = 'projectForm';
+
+  label.classList.add('label');
+  label.textContent = 'New project';
+
+  input.classList.add('input');
+  input.type = 'text';
+
+  button.classList.add('button', 'mt-5');
+  button.textContent = 'Create';
+  button.id = 'addProject';
+  form.append(label, input, button);
+
+  return form;
 };
 
 export {
-  top, container, navigation, main, form, fullPage, project, task,
+  top, container, navigation, main, form, fullPage, projectLink, task, projectForm,
 };
