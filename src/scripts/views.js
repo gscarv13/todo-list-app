042c59fc1e9ea67a601a246fcec25a19523c0b6b
Gus <gsc13@tuta.io>
2021-05-21 18:27:51 -0300

    Add project form display, Update ID selectors

--- src/scripts/views.js
+++ src/scripts/views.js
@@ -1,27 +1,59 @@
 import * as Builder from './builders';
 
-const displayForm = () => {
-  const main = document.querySelector('main');
+const displayTaskForm = () => {
+  const container = document.querySelector('#formContainer');
 
   if (document.querySelector('#form') === null) {
     const form = Builder.form();
-    main.append(form);
+    container.append(form);
   }
-  return main;
+  return container;
 };
 
-const hideForm = (e) => {
+const hideTaskForm = (e) => {
   if (e.target.id === 'addTask') {
     const form = document.querySelector('#form');
     form.remove();
   }
 };
 
-const displayProjects = (projects) => {
-  const projectsContainer = document.querySelector('#task-list');
-  for (let i = 0; i < projects.length; i += 1) {
-    projectsContainer.appendChild(Builder.project(projects[i], projects));
+const displayProjectForm = () => {
+  const container = document.querySelector('#projectFormContainer');
+
+  if (document.querySelector('#projectForm') === null) {
+    const form = Builder.projectForm();
+    container.append(form);
+  }
+  return container;
+};
+
+const hideProjectForm = (e) => {
+  if (e.target.id === 'addProject') {
+    const form = document.querySelector('#projectForm');
+    form.remove();
   }
 };
 
-export { displayForm, hideForm, displayProjects };
+const displayProjects = (projects) => {
+  const projectsContainer = document.querySelector('#projectsList');
+  for (let i = 0; i < projects.length; i += 1) {
+    projectsContainer.appendChild(Builder.projectLink(projects[i], projects));
+  }
+};
+
+const displayTask = (task) => {
+  const main = document.querySelector('#tasksContainer');
+  main.append(task);
+};
+
+const displayAllTasks = (project) => {
+  for (let i = 0; i < project.taskList.length; i += 1) {
+    const task = Builder.task(project.taskList[i]);
+    displayTask(task);
+  }
+};
+
+export {
+  displayTaskForm, hideTaskForm, displayProjects, displayTask,
+  displayAllTasks, displayProjectForm, hideProjectForm,
+};
